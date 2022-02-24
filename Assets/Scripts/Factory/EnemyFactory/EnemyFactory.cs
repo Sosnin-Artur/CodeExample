e93f7987c51ef4faffa74526d13ac8c23eedc695
Artur-Sasnin <Artur.Sasnin@itechart-group.com>
2022-02-24 12:24:31 +0300

    Create factory

---
+++ Assets/Scripts/Factory/EnemyFactory/EnemyFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+class EnemyFactory : IEnemyFactory
+{    
+    private readonly DiContainer _container;    
+    
+    public EnemyFactory(DiContainer container)
+    {        
+        _container = container;                
+    }
+
+    public BaseEnemyPresenter Create()
+    {                
+        return _container.Instantiate<EnemyPresenter>();
+    }
+}
+
