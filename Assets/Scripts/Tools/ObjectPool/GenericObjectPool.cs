74eccb021ed399770343d1dbed69510deb4fa2ec
Artur-Sasnin <Artur.Sasnin@itechart-group.com>
2022-02-28 11:09:26 +0300

    create interface for poolable objects

---
+++ Assets/Scripts/Tools/ObjectPool/GenericObjectPool.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+using ObjectPool;
+
+namespace ObjectPool
+{
+    public abstract class GenericObjectPool<T, TFactory>
+        where T : IPoolable
+        where TFactory : IFactory
+    {
+        protected readonly Queue<T> PooledObjects = new Queue<T>();
+
+        private readonly IFactory<T> _factory;
+
+        public GenericObjectPool(IFactory<T> factory, int count)
+        {
+            _factory = factory;
+            AddObjects(count);
+        }
+
+        public abstract T Get();
+        public abstract void Release(T objectToSet);
+
+        private void AddObjects(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                var newObject = _factory.Create();
+                Release(newObject);
+            }
+        }
+    }
+}
